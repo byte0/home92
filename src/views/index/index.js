983bce267f038324471209eb2a85c3011d650172
上下文 <1031247267@qq.com>
2020-03-24 14:54:37 +0800

    菜单布局实现

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -2,8 +2,14 @@
   主页模块
 */
 import React from 'react'
-import { Carousel } from 'antd-mobile'
+import { Carousel, Flex } from 'antd-mobile'
 import axios from 'axios'
+import './index.css'
+// 导入菜单图片
+import nav1 from '../../assets/images/nav-1.png'
+import nav2 from '../../assets/images/nav-2.png'
+import nav3 from '../../assets/images/nav-3.png'
+import nav4 from '../../assets/images/nav-4.png'
 
 class Index extends React.Component {
 
@@ -40,11 +46,44 @@ class Index extends React.Component {
     )
   }
 
+  renderMenu = () => {
+    const menuData = [{
+      id: 1,
+      title: '整租',
+      img: nav1
+    }, {
+      id: 2,
+      title: '合租',
+      img: nav2
+    }, {
+      id: 3,
+      title: '地图找房',
+      img: nav3
+    }, {
+      id: 4,
+      title: '去出租',
+      img: nav4
+    }]
+    const menuTag = menuData.map(item => (
+      <Flex.Item key={item.id}>
+        <img src={item.img} alt=""/>
+        <p>{item.title}</p>
+      </Flex.Item>
+    ))
+    return (
+      <Flex className='index-menu'>
+        {menuTag}
+      </Flex>
+    )
+  }
+
   render () {
     return (
       <div>
         {/*轮播图*/}
         {this.renderSwiper()}
+        {/*菜单*/}
+        {this.renderMenu()}
       </div>
     )
   }
