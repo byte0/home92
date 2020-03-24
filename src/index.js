28c4984565c9bbd10ee74029212e34678d85401a
上下文 <1031247267@qq.com>
2020-03-24 14:35:33 +0800

    轮播图数据对接

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import 'antd-mobile/dist/antd-mobile.css';
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -2,12 +2,49 @@
   主页模块
 */
 import React from 'react'
+import { Carousel } from 'antd-mobile'
+import axios from 'axios'
 
 class Index extends React.Component {
+
+  state = {
+    swiperData: []
+  }
+
+  // 加载轮播图接口数据
+  loadSwiper = async () => {
+    // axios.get('http://api-haoke-dev.itheima.net/home/swiper')
+    //  .then(res => {
+    //   this.setState({
+    //     swiperData: res.data.body
+    //   })
+    //  })
+    const res = await axios.get('http://api-haoke-dev.itheima.net/home/swiper')
+    this.setState({
+      swiperData: res.data.body
+    })
+  }
+
+  componentDidMount () {
+    this.loadSwiper()
+  }
+
+  renderSwiper = () => {
+    const swiperItems = this.state.swiperData.map(item => (
+      <img key={item.id} src={"http://api-haoke-dev.itheima.net" + item.imgSrc} alt=""/>
+    ))
+    return (
+      <Carousel dots infinite autoplay>
+        {swiperItems}
+      </Carousel>
+    )
+  }
+
   render () {
     return (
       <div>
-        <div>主页页面</div>
+        {/*轮播图*/}
+        {this.renderSwiper()}
       </div>
     )
   }
