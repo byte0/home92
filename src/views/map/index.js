b8651ed7e66466451e60ab9211fc4a88cbe31556
上下文 <1031247267@qq.com>
2020-03-28 09:48:37 +0800

    设置地图中心点坐标

--- src/views/map/index.js
+++ src/views/map/index.js
@@ -4,17 +4,30 @@
 import React from 'react'
 import './index.scss'
 import { NavBar, Icon } from 'antd-mobile'
+import { getCurrentCity } from '../../utils/config.js'
 
 class MapTest extends React.Component {
 
   // 初始化地图
-  initMap = () => {
-    // 1、创建地图实例对象
-    const map = new window.BMap.Map("mymap")
-    // 2、创建地图中心点坐标
-    const point = new window.BMap.Point(116.40387397,39.91488908)
-    // 3、设置地图的中心点坐标和缩放级别 
-    map.centerAndZoom(point, 9)
+  initMap = async () => {
+    // 获取当前城市信息
+    const city = await getCurrentCity()
+    // 根据城市名称获取城市的经纬度
+    const geo = new window.BMap.Geocoder()
+    // getPoint参数：（1、城市名称；2、回调函数；3、国家名称）
+    geo.getPoint(city.label, (data) => {
+      // 获取城市经纬度数据
+      let info = {
+        lng: data && data.lng,
+        lat: data && data.lat
+      }
+      // 1、创建地图实例对象
+      const map = new window.BMap.Map("mymap")
+      // 2、创建地图中心点坐标
+      const point = new window.BMap.Point(info.lng, info.lat)
+      // 3、设置地图的中心点坐标和缩放级别 
+      map.centerAndZoom(point, 11)
+    }, '中国')
   }
 
   componentDidMount () {
